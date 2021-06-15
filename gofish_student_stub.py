19a6c29b7ef0e32c2e6710bfe5aa542a990050ba
rgliuca <44307228+rgliuca@users.noreply.github.com>
2021-06-15 16:43:44 -0700

    Update gofish_student_stub.py

--- gofish_student_stub.py
+++ gofish_student_stub.py
@@ -1,14 +1,16 @@
 from blackjack import *
 
 class GoFishHand:
-    def __init__(self):
+    CARD_RANK = ["A", "2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+    def __init__(self, player="", cards=[]):
         # You need to keep track of two things in this class:
         #   1. the cards in this hand
         #   2. the rank of (4 of a kind, cards with the same values)  You can 
         #     compute the score from this.
         pass
     
-    def get_rank_cards(self, rank):
+    def remove_rank_cards(self, rank):
         # removes cards with the given rank from this "hand" and returns
         # the cards as a list to the caller
         # If there aren't any cards with the given rank, then return an
@@ -16,6 +18,7 @@ class GoFishHand:
         pass
     
     def add_cards(self, cards):
+        # cards is a list
         # Just like in BJHand, add cards to this "hand" but with an additional
         # function of checking if you have the full suit of a given rank
         pass
@@ -30,11 +33,10 @@ class GoFishHand:
         # you can use Card.get_value() for the rank value 
         pass
     
-    def print(col_size=5, show_cards=True):
+    def print(self, col_size=5, show_all_cards=True):
         # prints the cards in hand
         pass
-
-
+               
 if __name__ == "__main__":
   cards = CardDeck()
   cards.shuffle()
